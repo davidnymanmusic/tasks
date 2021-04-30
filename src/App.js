165f10352d987d14f9cacd19ae99606c0143ef2a
David Nyman <davenyman@gmail.com>
2021-04-30 12:32:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  let tasks = [
+    'Play with pets',
+    'Clean pantry 15 minutes',
+    'Check for recycling',
+    'Organize couch',
+    'Vacuum',
+    'Break down boxes',
+    'Weed',
+    'Check Trello',
+    'Wipe counters',
+    'Compost',
+    'Brush teeth',
+    'Pick up clothes',
+    'Make a list',
+    'Work on a playlist',
+    'Clean toilet'
+  ];
+
+  let allTasks = tasks;
+
+
+  const [activeTask, setactiveTask] = useState(0);
+  const [style, setStyle] = useState('.null');
+
+
+  const randomTask = () => {
+    const len = tasks.length;
+          setStyle('strike');
+    setTimeout(() => {
+  setactiveTask(Math.floor(Math.random() * len));
+      setStyle('.null');
+}, 1000);
+
+  };
+  const nope = () => {
+    const index = tasks[activeTask];
+
+    tasks = tasks.filter((item) => item !== index);
+
+    console.log(tasks);
+  };
+
+  const reset = () => {
+    tasks = allTasks;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class={style} >{tasks[activeTask]}</div>
+      <button onClick={randomTask}> not now</button>
+
+    </>
   );
 }
 
